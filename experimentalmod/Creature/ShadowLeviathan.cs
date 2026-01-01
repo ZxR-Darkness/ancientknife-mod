37d2a749b394237dd4577564163e979418afaa7f
ZxR-Darkness <imperium.foxes1@gmail.com>
2026-01-01 23:02:12 +0300

    added creature

---
+++ experimentalmod/Creature/ShadowLeviathan.cs
@@ -0,0 +1,70 @@
+﻿using ECCLibrary;
+using ECCLibrary.Data;
+using Nautilus.Assets;
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace experimentalmod.Items
+{
+    // Компонент для атаки: при касании игрока выводит сообщение
+    internal class ShadowMeleeAttack : MeleeAttack
+    {
+        public override void OnTouch(Collider collider)
+        {
+            base.OnTouch(collider);
+            ErrorMessage.AddMessage("Shadow Leviathan");
+        }
+    }
+
+    public class ShadowLeviathan : CreatureAsset
+    {
+        public ShadowLeviathan(PrefabInfo info) : base(info) { }
+
+        protected override CreatureTemplate CreateTemplate()
+        {
+
+            GameObject model = StaticStructures.Bundle.LoadAsset<GameObject>("Assets/Worm_Root.prefab");
+
+            // 2. Создаем шаблон (модель, тип поведения, тип цели, радиус патрулирования)
+            var template = new CreatureTemplate(model, BehaviourType.Leviathan, EcoTargetType.Leviathan, 300f)
+            {
+                CellLevel = LargeWorldEntity.CellLevel.Far,
+                Mass = 5000,
+                EyeFOV = -0.75f,
+                AcidImmune = true,
+
+                // Настройка физики движения
+                LocomotionData = new LocomotionData(5f, 0.2f),
+
+                // Синхронизация скорости с аниматором
+                AnimateByVelocityData = new AnimateByVelocityData(15f),
+
+                // Настройки случайного плавания
+                SwimRandomData = new SwimRandomData(0.2f, 10f, new Vector3(50, 20, 50), 4f, 1f, true),
+
+                // Настройки ИИ атаки
+                AttackLastTargetData = new AttackLastTargetData(0.8f, 15f, 0.6f, 10f)
+            };
+
+            // Добавляем агрессию при обнаружении цели (игрок/рыбы)
+            template.AddAggressiveWhenSeeTargetData(new AggressiveWhenSeeTargetData(EcoTargetType.Shark, 2, 100, 3));
+
+            return template;
+        }
+
+        protected override IEnumerator ModifyPrefab(GameObject prefab, CreatureComponents components)
+        {
+            // 1. Поиск рта для атаки
+            var mouth = prefab.transform.SearchChild("Mouth");
+
+            if (mouth != null)
+            {
+                // Добавляем компонент атаки
+                CreaturePrefabUtils.AddMeleeAttack<ShadowMeleeAttack>(prefab, components, mouth.gameObject, true, 50f);
+            }
+
+            yield break;
+        }
+    }
+}
--- experimentalmod/Plugin.cs
+++ experimentalmod/Plugin.cs
@@ -1,11 +1,13 @@
-﻿using System.Reflection;
-using BepInEx;
+﻿using BepInEx;
 using BepInEx.Logging;
-using experimentalmod.Items.Equipment;
 using experimentalmod.Items;
+using experimentalmod.Items.Equipment;
 using experimentalmod.Items.Minerals;
 using HarmonyLib;
+using Nautilus.Assets;
 using System.IO;
+using System.Reflection;
+using UnityEngine;
 
 namespace experimentalmod
 {
@@ -15,19 +17,34 @@ namespace experimentalmod
     {
         public new static ManualLogSource Logger { get; private set; }
         private static Assembly Assembly { get; } = Assembly.GetExecutingAssembly();
+        private static string AssetsFolder => Path.Combine(Path.GetDirectoryName(Assembly.Location), "Assets");
 
         private void Awake()
         {
             Logger = base.Logger;
+            string bundlePath = Path.Combine(AssetsFolder, "myassetbundle");
+
+            if (File.Exists(bundlePath))
+            {
+                StaticStructures.Bundle = AssetBundle.LoadFromFile(bundlePath);
+                Logger.LogInfo("Бандл успешно загружен в память!");
+            }
+            else
+            {
+                Logger.LogError($"Файл бандла не найден! Проверь путь: {bundlePath}");
+                return;
+            }
 
             InitializePrefabs();
-
             Harmony.CreateAndPatchAll(Assembly, $"{PluginInfo.PLUGIN_GUID}");
             Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
         }
 
         private void InitializePrefabs()
         {
+            var shadowLevInfo = PrefabInfo.WithTechType("ShadowLev", "Shadow Leviathan", "Теневой ужас.");
+            var shadowLev = new ShadowLeviathan(shadowLevInfo);
+            shadowLev.Register();
             UnknownMinerales.Register();
             StaticStructures.Register();
             TechKnifePrefab.Register();
